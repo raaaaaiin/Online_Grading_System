f008e511b6bf262b6cdca9b982734bea7e6861fa
Marc Raineer Filosopo <marcraineer0089@gmail.com>
2023-05-11 01:04:18 +0800

    Updated import export Student

--- Admin/importData.php
+++ Admin/importData.php
@@ -58,14 +58,64 @@ if(isset($_POST['importSubmit'])){
             // Close opened CSV file
             fclose($csvFile);
             
-            $qstring = '?status=succ';
+            echo '<div id="custom-dialog" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); display: flex; justify-content: center; align-items: center; z-index: 1000;">
+            <div style="background: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            text-align: center; width: 300px; max-width: 90%;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="#32CD32" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle">
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+                <h2 style="margin-top: 0;">Success</h2>
+                <p>Data imported succesfully</p>
+                <button id="ok-button" style="background: #3085d6; color: #fff; border: none; padding: 10px 20px; border-radius: 3px; cursor: pointer; font-size: 14px; margin-top: 10px;">OK</button>
+            </div>
+        </div>';
+    
+        echo '<script>
+            document.getElementById("ok-button").addEventListener("click", function () {
+                window.location.href = "teacher.php";
+            });
+        </script>';
         }else{
             $qstring = '?status=err';
+            
+        echo '<div id="custom-dialog" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); display: flex; justify-content: center; align-items: center; z-index: 1000;">
+        <div style="background: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); text-align: center; width: 300px; max-width: 90%;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="#FF0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle">
+                <circle cx="12" cy="12" r="10"></circle>
+                <line x1="15" y1="9" x2="9" y2="15"></line>
+                <line x1="9" y1="9" x2="15" y2="15"></line>
+            </svg>
+            <h2 style="margin-top: 0;">Error</h2>
+            <p>Failed to import data</p>
+            <button id="ok-button" style="background: #3085d6; color: #fff; border: none; padding: 10px 20px; border-radius: 3px; cursor: pointer; font-size: 14px; margin-top: 10px;">OK</button>
+        </div>
+    </div>';
+    
+        echo '<script>
+            document.getElementById("ok-button").addEventListener("click", function () {
+                window.location.href = "teacher.php";
+            });
+        </script>';
         }
     }else{
-        $qstring = '?status=invalid_file';
+        echo '<div id="custom-dialog" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); display: flex; justify-content: center; align-items: center; z-index: 1000;">
+        <div style="background: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); text-align: center; width: 300px; max-width: 90%;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="#FFA500" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-circle">
+                <circle cx="12" cy="12" r="10"></circle>
+                <line x1="12" y1="8" x2="12" y2="12"></line>
+                <line x1="12" y1="16" x2="12" y2="16"></line>
+            </svg>
+            <h2 style="margin-top: 0;">Oops!</h2>
+            <p>File is invalid format</p>
+            <button id="ok-button" style="background: red; color: #fff; border: none; padding: 10px 20px; border-radius: 3px; cursor: pointer; font-size: 14px; margin-top: 10px;">OK</button>
+        </div>
+    </div>';
+    
+        echo '<script>
+            document.getElementById("ok-button").addEventListener("click", function () {
+                window.location.href = "teacher.php";
+            });
+        </script>';
     }
 }
-
-// Redirect to the listing page
-header("Location: student.php".$qstring);
